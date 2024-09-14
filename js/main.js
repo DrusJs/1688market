8ecf4643f7b1848a40532dffc3c9b9b7b229174f
drusjs <magnojoky962@gmail.com>
2024-09-14 16:21:05 +0300

    feat: add basket

--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const searchWrapper = document.querySelector('.search-wrapper')
-
-    if (searchWrapper) {
-        searchWrapper.addEventListener('click', (e)=>{
-            e.currentTarget.classList.add('active')
-        })
-    }
-
-
     const catalogWrapper = document.querySelector('.catalog-wrapper')
     const catalogButton = document.querySelector('.catalog-button')
     const catalogMainItems = document.querySelectorAll('.catalog-list .catalog-item')
@@ -72,21 +63,56 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 
-    document.addEventListener('click', function(event) {
-        const isClick = searchWrapper.contains(event.target);
-    
-        if (!isClick) {
-            searchWrapper.classList.remove('active');
-        }
-    });
+    const countersButtons = document.querySelectorAll('.counter .btn')
+
+    if (countersButtons.length > 0) {
+        countersButtons.forEach(button => {
+            console.log(button)
+            button.addEventListener('click', function () {
+                handleCounterAction(this);
+            });
+        });
+    }
 
 
+    const searchWrapper = document.querySelector('.search-wrapper')
+
+    if (searchWrapper) {
+        searchWrapper.addEventListener('click', (e)=>{
+            e.currentTarget.classList.add('active')
+        })
+
+        //скрытие поиска при нажатии вне элемента
+        document.addEventListener('click', function(event) {
+            const isClick = searchWrapper.contains(event.target);
+        
+            if (!isClick) {
+                searchWrapper.classList.remove('active');
+            }
+        });
+    }
 });
 
 function validInputNumber(event) {
     return(event.charCode >= 48 && event.charCode <= 57 && /^\d{0,3}$/.test(this.value))
 }
 
-function sliderUpdate(element, value, isFirst) {
-    element.parentElement.nextElementSibling.noUiSlider.set([isFirst?value:null, isFirst?null:value]);
+function handleCounterAction(item) {
+    let counterValue = item.parentElement.querySelector('.counter-value');
+    let currentValue = parseInt(counterValue.innerHTML);
+
+    if (isNaN(currentValue)) {
+        console.error("Counter NaN = " + currentValue);
+        return;
+    }
+
+    if (item.classList.contains('cnt-plus')) {
+        counterValue.innerHTML = currentValue + 1;
+    } else if (item.classList.contains('cnt-minus') && currentValue > 0) {
+        counterValue.innerHTML = currentValue - 1;
+    }
+}
+
+function handleBasketCheckboxUpdate() {
+
 }
